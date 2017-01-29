005ba2c14dad68f038e5df94b5f3fb25bf8f7886
Sebastian Geisler <sgeisler@wh2.tu-dresden.de>
2017-01-29 23:44:30 +0100

    add tests for half candles

--- test_graph.py
+++ test_graph.py
@@ -42,3 +42,29 @@ class BloomfilterTest(TestCase):
         self.assertEquals(CandleStickGraph.SYMBOL_CANDLE, first_candle[1])
         self.assertEquals(CandleStickGraph.SYMBOL_HALF_BOTTOM, first_candle[2])
         self.assertEquals(CandleStickGraph.SYMBOL_STICK, first_candle[3])
+
+    def test_candle_half_2(self):
+        c = Candle(1.3, 4, 1.5, 3.7)
+        c_fake = Candle(0, 4, 0, 0)
+        g = CandleStickGraph([c, c_fake], 4)
+        drawn_string = g.draw(False)
+
+        lines = drawn_string.split("\n")
+        first_candle = [line[9] for line in lines[1:-1]]
+        self.assertEquals(CandleStickGraph.SYMBOL_HALF_TOP, first_candle[0])
+        self.assertEquals(CandleStickGraph.SYMBOL_CANDLE, first_candle[1])
+        self.assertEquals(CandleStickGraph.SYMBOL_HALF_CANDLE_BOTTOM, first_candle[2])
+        self.assertEquals(CandleStickGraph.SYMBOL_NOTHING, first_candle[3])
+
+    def test_candle_half_3(self):
+        c = Candle(1.2, 4, 1.5, 3.8)
+        c_fake = Candle(0, 4, 0, 0)
+        g = CandleStickGraph([c, c_fake], 4)
+        drawn_string = g.draw(False)
+
+        lines = drawn_string.split("\n")
+        first_candle = [line[9] for line in lines[1:-1]]
+        self.assertEquals(CandleStickGraph.SYMBOL_CANDLE, first_candle[0])
+        self.assertEquals(CandleStickGraph.SYMBOL_CANDLE, first_candle[1])
+        self.assertEquals(CandleStickGraph.SYMBOL_HALF_BOTTOM, first_candle[2])
+        self.assertEquals(CandleStickGraph.SYMBOL_NOTHING, first_candle[3])
