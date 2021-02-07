d5665248b7d596cabe0a52eb8ab7b639e0847c51
femtotrader <femto.trader@gmail.com>
2021-02-07 15:41:41 +0100

    Move tests from unittest to pytest

--- tests/test_termgraph.py
+++ tests/test_termgraph.py
@@ -1,71 +1,69 @@
-from unittest import TestCase
 from termgraph import CandleStickGraph, Candle
 
 
-class CandleTests(TestCase):
-    def test_candle_int_4(self):
-        c = Candle(1, 4, 0, 3)
-        g = CandleStickGraph([c], 4)
-        drawn_string = g.draw(False)
+def test_candle_int_4():
+    c = Candle(1, 4, 0, 3)
+    g = CandleStickGraph([c], 4)
+    drawn_string = g.draw(False)
 
-        lines = drawn_string.split("\n")
-        first_candle = [line[9] for line in lines[1:-1]]
+    lines = drawn_string.split("\n")
+    first_candle = [line[9] for line in lines[1:-1]]
 
-        self.assertEqual(CandleStickGraph.SYMBOL_STICK, first_candle[0])
-        self.assertEqual(CandleStickGraph.SYMBOL_CANDLE, first_candle[1])
-        self.assertEqual(CandleStickGraph.SYMBOL_CANDLE, first_candle[2])
-        self.assertEqual(CandleStickGraph.SYMBOL_STICK, first_candle[3])
+    assert CandleStickGraph.SYMBOL_STICK == first_candle[0]
+    assert CandleStickGraph.SYMBOL_CANDLE == first_candle[1]
+    assert CandleStickGraph.SYMBOL_CANDLE == first_candle[2]
+    assert CandleStickGraph.SYMBOL_STICK == first_candle[3]
 
-    def test_candle_frac_8(self):
-        c = Candle(1.5, 4, 0, 3.5)
-        g = CandleStickGraph([c], 8)
-        drawn_string = g.draw(False)
+def test_candle_frac_8():
+    c = Candle(1.5, 4, 0, 3.5)
+    g = CandleStickGraph([c], 8)
+    drawn_string = g.draw(False)
 
-        lines = drawn_string.split("\n")
-        first_candle = [line[9] for line in lines[1:-1]]
-        self.assertEqual(CandleStickGraph.SYMBOL_STICK, first_candle[0])
-        self.assertEqual(CandleStickGraph.SYMBOL_CANDLE, first_candle[1])
-        self.assertEqual(CandleStickGraph.SYMBOL_CANDLE, first_candle[2])
-        self.assertEqual(CandleStickGraph.SYMBOL_CANDLE, first_candle[3])
-        self.assertEqual(CandleStickGraph.SYMBOL_CANDLE, first_candle[4])
-        self.assertEqual(CandleStickGraph.SYMBOL_STICK, first_candle[5])
-        self.assertEqual(CandleStickGraph.SYMBOL_STICK, first_candle[6])
-        self.assertEqual(CandleStickGraph.SYMBOL_STICK, first_candle[7])
+    lines = drawn_string.split("\n")
+    first_candle = [line[9] for line in lines[1:-1]]
+    assert CandleStickGraph.SYMBOL_STICK == first_candle[0]
+    assert CandleStickGraph.SYMBOL_CANDLE == first_candle[1]
+    assert CandleStickGraph.SYMBOL_CANDLE == first_candle[2]
+    assert CandleStickGraph.SYMBOL_CANDLE == first_candle[3]
+    assert CandleStickGraph.SYMBOL_CANDLE == first_candle[4]
+    assert CandleStickGraph.SYMBOL_STICK == first_candle[5]
+    assert CandleStickGraph.SYMBOL_STICK == first_candle[6]
+    assert CandleStickGraph.SYMBOL_STICK == first_candle[7]
 
-    def test_candle_half_1(self):
-        c = Candle(1.5, 4, 0, 3.5)
-        g = CandleStickGraph([c], 4)
-        drawn_string = g.draw(False)
+def test_candle_half_1():
+    c = Candle(1.5, 4, 0, 3.5)
+    g = CandleStickGraph([c], 4)
+    drawn_string = g.draw(False)
 
-        lines = drawn_string.split("\n")
-        first_candle = [line[9] for line in lines[1:-1]]
-        self.assertEqual(CandleStickGraph.SYMBOL_HALF_TOP, first_candle[0])
-        self.assertEqual(CandleStickGraph.SYMBOL_CANDLE, first_candle[1])
-        self.assertEqual(CandleStickGraph.SYMBOL_HALF_BOTTOM, first_candle[2])
-        self.assertEqual(CandleStickGraph.SYMBOL_STICK, first_candle[3])
+    lines = drawn_string.split("\n")
+    first_candle = [line[9] for line in lines[1:-1]]
+    assert CandleStickGraph.SYMBOL_HALF_TOP == first_candle[0]
+    assert CandleStickGraph.SYMBOL_CANDLE == first_candle[1]
+    assert CandleStickGraph.SYMBOL_HALF_BOTTOM == first_candle[2]
+    assert CandleStickGraph.SYMBOL_STICK == first_candle[3]
 
-    def test_candle_half_2(self):
-        c = Candle(1.5, 4, 1.3, 3.7)
-        c_fake = Candle(0, 4, 0, 0)
-        g = CandleStickGraph([c, c_fake], 4)
-        drawn_string = g.draw(False)
+def test_candle_half_2():
+    c = Candle(1.5, 4, 1.3, 3.7)
+    c_fake = Candle(0, 4, 0, 0)
+    g = CandleStickGraph([c, c_fake], 4)
+    drawn_string = g.draw(False)
 
-        lines = drawn_string.split("\n")
-        first_candle = [line[9] for line in lines[1:-1]]
-        self.assertEqual(CandleStickGraph.SYMBOL_HALF_TOP, first_candle[0])
-        self.assertEqual(CandleStickGraph.SYMBOL_CANDLE, first_candle[1])
-        self.assertEqual(CandleStickGraph.SYMBOL_HALF_CANDLE_BOTTOM, first_candle[2])
-        self.assertEqual(CandleStickGraph.SYMBOL_NOTHING, first_candle[3])
+    lines = drawn_string.split("\n")
+    first_candle = [line[9] for line in lines[1:-1]]
+    assert CandleStickGraph.SYMBOL_HALF_TOP == first_candle[0]
+    assert CandleStickGraph.SYMBOL_CANDLE == first_candle[1]
+    assert CandleStickGraph.SYMBOL_HALF_CANDLE_BOTTOM == first_candle[2]
+    assert CandleStickGraph.SYMBOL_NOTHING == first_candle[3]
 
-    def test_candle_half_3(self):
-        c = Candle(1.5, 4, 1.2, 3.8)
-        c_fake = Candle(0, 4, 0, 0)
-        g = CandleStickGraph([c, c_fake], 4)
-        drawn_string = g.draw(False)
+def test_candle_half_3():
+    c = Candle(1.5, 4, 1.2, 3.8)
+    c_fake = Candle(0, 4, 0, 0)
+    g = CandleStickGraph([c, c_fake], 4)
+    drawn_string = g.draw(False)
 
-        lines = drawn_string.split("\n")
-        first_candle = [line[9] for line in lines[1:-1]]
-        self.assertEqual(CandleStickGraph.SYMBOL_CANDLE, first_candle[0])
-        self.assertEqual(CandleStickGraph.SYMBOL_CANDLE, first_candle[1])
-        self.assertEqual(CandleStickGraph.SYMBOL_HALF_BOTTOM, first_candle[2])
-        self.assertEqual(CandleStickGraph.SYMBOL_NOTHING, first_candle[3])
+    lines = drawn_string.split("\n")
+    first_candle = [line[9] for line in lines[1:-1]]
+    assert CandleStickGraph.SYMBOL_CANDLE == first_candle[0]
+    assert CandleStickGraph.SYMBOL_CANDLE == first_candle[1]
+    assert CandleStickGraph.SYMBOL_HALF_BOTTOM == first_candle[2]
+    assert CandleStickGraph.SYMBOL_NOTHING == first_candle[3]
